03d096c3c50edfd3d295d39e3b1cff1dcddfffd2
dominikmascherbauer <dominik.mascherbauer@gmail.com>
2024-12-01 19:54:33 +0100

    Cleanup

--- kotlin/src/Day01.kt
+++ kotlin/src/Day01.kt
@@ -2,24 +2,22 @@ import kotlin.math.abs
 
 fun main() {
 
-    fun List<String>.asColumnListPair(): Pair<List<Int>, List<Int>> =
-        fold(mutableListOf<Int>() to mutableListOf<Int>()) { acc, s ->
-            acc.first.add(s.split(' ').first().toInt())
-            acc.second.add(s.split(' ').last().toInt())
-            acc
-        }
-
     fun Pair<List<Int>, List<Int>>.part1(): Int =
-        first.sorted()
-            .zip(second.sorted()) { a, b -> abs(a - b) }
-            .sum()
+        first.sorted().zip(second.sorted())
+            .sumOf { abs(it.first - it.second) }
 
     fun Pair<List<Int>, List<Int>>.part2(): Int =
         with(second.groupingBy { it }.eachCount()) {
             first.sumOf { it * this.getOrElse(it) { 0 } }
         }
 
-    val input = readInput("Day01").filterNot { it.isEmpty() }.asColumnListPair()
+    // Process input string
+    val input = readInput("Day01")
+        .filter { it.isNotEmpty() }
+        .map { line -> line.substringBefore(' ').toInt() to line.substringAfterLast(' ').toInt() }
+        .unzip()
+
+    // run task implementations
     println(input.part1())
     println(input.part2())
 }
