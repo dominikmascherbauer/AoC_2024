de02fedf493d0d9efd99434e0345d8bd2caa4566
dominikmascherbauer <dominik.mascherbauer@gmail.com>
2024-12-07 11:07:11 +0100

    Improve Day07 implementation

--- kotlin/src/Day07.kt
+++ kotlin/src/Day07.kt
@@ -1,22 +1,28 @@
+import java.util.function.BiFunction
+
 fun main() {
 
-    fun List<String>.part1(): Long =
+    fun List<String>.validCombinationSum(vararg ops: BiFunction<Long, Long, Long>): Long =
         map { line -> line.substringBefore(':').toLong() to line.substringAfter(':').split(' ').filter { it.isNotEmpty() }.map { it.toLong() } }
             .filter { line ->
-                line.second.drop(1).fold(listOf(line.second.first())) { acc, i ->
-                    acc.flatMap { listOf(it + i, it * i) }.filter { it <= line.first }
-                }.count { it == line.first } > 0
+                line.second.reversed().fold(listOf(line.first)) { acc, i ->
+                    acc.flatMap { num -> ops.map { it.apply(num, i) } }.filter { it >= 0 }
+                }.any { it == 0L }
             }
             .sumOf { it.first }
 
+    fun List<String>.part1(): Long =
+        validCombinationSum(
+            { a, b -> a - b },
+            { a, b -> if (a % b != 0L) -1 else a / b}
+        )
+
     fun List<String>.part2(): Long =
-        map { line -> line.substringBefore(':').toLong() to line.substringAfter(':').split(' ').filter { it.isNotEmpty() }.map { it.toLong() } }
-            .filter { line ->
-                line.second.drop(1).fold(listOf(line.second.first())) { acc, i ->
-                    acc.flatMap { listOf(it + i, it * i, "$it$i".toLong()) }.filter { it <= line.first }
-                }.count { it == line.first } > 0
-            }
-            .sumOf { it.first }
+        validCombinationSum(
+            { a, b -> a - b },
+            { a, b -> if (a % b != 0L) -1 else a / b},
+            { a, b -> if (!"$a".endsWith("$b")) -1 else "0$a".dropLast("$b".length).toLong() }
+        )
 
     // Process input string
     val input = readInput("Day07")
