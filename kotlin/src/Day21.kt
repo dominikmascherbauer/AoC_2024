e4f10f75d4267b7038f7c673521c76e0deecf7e0
dominikmascherbauer <dominik.mascherbauer@gmail.com>
2025-11-21 00:08:24 +0100

    finish Day21

---
+++ kotlin/src/Day21.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    fun List<String>.part1(): Any =
+        0
+
+    fun List<String>.part2(): Any =
+        0
+
+
+    val input = readInput("Day00")
+    println(input.part1())
+    println(input.part2())
+}
